4d9917d8058b798098dca32f5f01c9179c859b2f
kyungsun choi <kschoi0308@gmail.com>
2021-06-23 06:18:42 +0900

    add scroll event

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import throttle from 'lodash/throttle'
+import { motion, useMotionValue } from 'framer-motion'
 
 function App() {
-  return <div className="App">app</div>;
+  const y = useMotionValue(0)
+  const [lastYPos, setLastPos] = useState(0)
+
+  const handleScroll = throttle(
+    () => {
+      console.log('scrolled')
+    },
+    400,
+    { leading: true },
+  )
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, false)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll, false)
+    }
+  }, [lastYPos])
+
+  return (
+    <div className="min-h-screen bg-gray-500">
+      <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
+        <div style={{ height: '200vh' }}>Page</div>
+      </motion.div>
+    </div>
+  )
 }
 
-export default App;
+export default App
